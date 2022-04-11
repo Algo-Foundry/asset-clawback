ce912cf4881a869df4e4d3a595668d26b51b4392
Charlie Kong <charlie@undercurrent.tech>
2022-04-11 18:39:18 +0800

    clawback assignment

---
+++ README.md
@@ -0,0 +1,20 @@
+## Rekey account assignment
+
+Perform an asset clawback from one account to another. You are required to do the following,
+
+1. Create the asset. It can be an NFT or a fungible token.
+2. Send this asset to an account.
+3. Perform a clawback of this asset and send it to another account.
+
+Write your solution in the skeleton code provided in `clawback.js`
+
+### Setup instructions
+1. Install packages with `npm install`.
+
+### Running your script
+2. Run your script with `node clawback.js`.
+
+### Key points to remember
+1. The receiver accounts should have sufficient minimum balance.
+2. Accounts need to be opted into the asset before receiving it.
+3. You should set a clawback address to the asset.
---
+++ clawback.js
@@ -0,0 +1,32 @@
+const algosdk = require("algosdk");
+
+const algodClient = new algosdk.Algodv2(
+  process.env.ALGOD_TOKEN,
+  process.env.ALGOD_SERVER,
+  process.env.ALGOD_PORT
+);
+
+const creator = algosdk.mnemonicToSecretKey(process.env.MNEMONIC_CREATOR);
+
+const submitToNetwork = async (signedTxn) => {
+  // send txn
+  let tx = await algodClient.sendRawTransaction(signedTxn).do();
+  console.log("Transaction : " + tx.txId);
+
+  // Wait for transaction to be confirmed
+  confirmedTxn = await algosdk.waitForConfirmation(algodClient, tx.txId, 4);
+
+  //Get the completed Transaction
+  console.log(
+    "Transaction " +
+      tx.txId +
+      " confirmed in round " +
+      confirmedTxn["confirmed-round"]
+  );
+
+  return confirmedTxn;
+};
+
+(async () => {
+  // Write your code here
+})();
